8c18c012fbfb4a6b9dd5f2977ac0ad01adba04d4
Taras Nesterenko <tnestere@gmail.com>
2024-03-12 12:11:25 +0100

    Added Info component, conditional rendering for Cart items

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,43 +1,62 @@
 import React from 'react';
 import { CgCloseR } from 'react-icons/cg';
+import Info from '../Info/Info';
 
 const Drawer = ({ onClose, items = [], onRemove }) => {
+  const [orderId, setOrderId] = React.useState(null);
+  const [isOrderComplete, setIsOrderComplete] = React.useState(false);
+
   return (
     <div className="drawer-overlay">
       <div className="drawer">
         <h2 className="d-flex justify-between">
           Cart <CgCloseR onClick={onClose} className="cart-remove" />
         </h2>
-        {items.map((item) => (
-          <div key={item.id} className="cartItem d-flex align-center mb-20">
-            <img className="mr-10" width={150} height={150} src={item.img} alt="Sneakers" />
-            <div className="mr-10">
-              <p className="sex">Men's sneakers</p>
-              <p className="title">{item.title}</p>
-              <b>€ {item.price}</b>
+
+        {items.length > 0 ? (
+          <>
+            {items.map((item) => (
+              <div key={item.id} className="cartItem d-flex align-center mb-20">
+                <img className="mr-10" width={150} height={150} src={item.img} alt="Sneakers" />
+                <div className="mr-10">
+                  <p className="sex">Men's sneakers</p>
+                  <p className="title">{item.title}</p>
+                  <b>€ {item.price}</b>
+                </div>
+                <CgCloseR onClick={() => onRemove(item.id)} className="cart-remove" />
+              </div>
+            ))}
+            <div className="cart-items">
+              <div className="cartTotalBlock">
+                <ul>
+                  <li>
+                    <span>Total: </span>
+                    <div></div>
+                    <b>€ 430,00</b>
+                  </li>
+                  <li>
+                    <span>Tax 5%: </span>
+                    <div></div>
+                    <b>€ 21,05</b>
+                  </li>
+                </ul>
+                <button className="greenButton">
+                  Checkout <img src="/img/arrow.svg" alt="Arrow" />
+                </button>
+              </div>
             </div>
-            <CgCloseR onClick={() => onRemove(item.id)} className="cart-remove" />
-          </div>
-        ))}
-        <div className="cart-items">
-          <div className="cartTotalBlock">
-            <ul>
-              <li>
-                <span>Total: </span>
-                <div></div>
-                <b>€ 430,00</b>
-              </li>
-              <li>
-                <span>Tax 5%: </span>
-                <div></div>
-                <b>€ 21,05</b>
-              </li>
-            </ul>
-            <button className="greenButton">
-              Checkout <img src="/img/arrow.svg" alt="Arrow" />
-            </button>
-          </div>
-        </div>
+          </>
+        ) : (
+          <Info
+            title={isOrderComplete ? 'Order placed!' : 'Cart is empty'}
+            description={
+              isOrderComplete
+                ? `Your order #${orderId} will be transferred to courier delivery soon`
+                : 'Add at least one pair of sneakers to place an order.'
+            }
+            image={isOrderComplete ? 'img/complete-order.jpg' : 'img/empty-cart.jpg'}
+          />
+        )}
       </div>
     </div>
   );
